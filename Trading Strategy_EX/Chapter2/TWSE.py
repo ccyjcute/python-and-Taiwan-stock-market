eefac7d4c13da54b78d465358f7afd2fbc43a845
yangcc <yangturtlecc58@gmail.com>
2024-03-11 23:18:04 +0800

    update TWSE.py

--- Trading Strategy_EX/Chapter2/TWSE.py
+++ Trading Strategy_EX/Chapter2/TWSE.py
@@ -3,11 +3,11 @@ import json
 import pandas as pd
 def twse_data(r_date:str):
     #一樣我們對api進行請求
-    data = requests.get(f'https://www.twse.com.tw/fund/T86?response=json&date={r_date}&selectType=ALLBUT0999&_=1614316365630')
+    data = requests.get(f'https://www.twse.com.tw/rwd/zh/fund/T86?date={r_date}&selectType=ALL&response=json&_=1710163649133')
     #使用json套件將他loads成json格式之後處理
     data_json = json.loads(data.text)
     #我們知道了欄位是fields，資料是data
     data_store = pd.DataFrame(data_json['data'],columns=data_json['fields'])
     return data_store
-
+twse_data('20240311')
 
