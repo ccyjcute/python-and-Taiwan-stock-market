37cc6ede23c7051cf3657e4ca007f7f0952fefe4
yangcc <yangturtlecc58@gmail.com>
2024-03-12 19:44:14 +0800

    update 2_bwd.py

--- Trading/2_buy_with_devidend.py
+++ Trading/2_buy_with_devidend.py
@@ -11,40 +11,33 @@ try:
     stock_list = pd.read_excel('./stock_list.xlsx') # 讀取台股列表
     all_stock = stock_list['代號'].values # 獲取所有的股票代號(.value是將pandas轉成numpy)
     print(all_stock)
-    #儲存每支股票的殖利率
-    dividend_store = []
+    
+    dividend_store = [] # 儲存每支股票的殖利率
     stock_store = []
-    #計數用
-    count = 0
+    count = 0 # 計數用
     #迴圈loop每一支股票
     for i in all_stock:
-        #我們來計算一下每一筆處理的時間，在開頭記錄一個start時間點
-        start = time.time()
-        #計數用參數
-        count+=1
-        #yfinance呼叫該股Ticker類
-        stock = yf.Ticker(f'{i}.TW')
-        #info中具備殖利率信息，拿來使用，並排除None
+        start = time.time() # 計算每一筆處理的時間，在開頭記錄一個start時間點
+        count+=1 # 計數用參數
+ 
+        stock = yf.Ticker(f'{i}.TW') # info的殖利率是從第一季的配息預估整年的配息
         try:
-            if stock.info['dividendYield'] !=None:
-                #有的話取殖利率
-                d_y = stock.info['dividendYield']
-                #有時候yfinance回傳的資料有異常需做排除，如果值不是0且大於0.05的才儲存
-                if d_y!=None and d_y>=0.05:
+            d_y = stock.info['dividendYield']
+            if d_y !=None and d_y>=0.05: # 殖利率大於5%
                     stock_store.append(i)
                     dividend_store.append(d_y)
             else:
                 d_y=None
-            #記錄每一筆結束時間
-            end = time.time()
-            #將進度print出來
+            
+            end = time.time() # 記錄每一筆結束時間
+            
             print(f'Dealing: {count} | All: {len(all_stock)} | Stock: {i} | DY: {d_y} | Cost Time: {end-start}s')
         except:
             print(f'Error Stock ! Dealing: {count} | All: {len(all_stock)} | Stock: {i}')
     data = pd.DataFrame()
     data['代號'] = stock_store
     data['殖利率'] = dividend_store
-    data.to_excel('D:\Trading\dividend_list.xlsx')
+    data.to_excel('dividend_list.xlsx')
 except SystemExit:
     print('Its OK')
 except:
